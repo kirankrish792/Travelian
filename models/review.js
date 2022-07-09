301c230daaf051c834afaad971933e77b54ca2b6
Kiran KS <kirankrish792@gmail.com>
2022-07-09 10:26:59 +0530

    Issues Fixed

--- models/review.js
+++ models/review.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const reviewSchema = new Schema({
-<<<<<<< HEAD
   body: String,
   rating: Number,
   author: {
@@ -23,37 +22,4 @@ const reviewSchema = new Schema({
 });
 
 
-=======
-    body: String,
-    rating: Number,
-    author: {
-        type: Schema.Types.ObjectId,
-        ref: 'User'
-    },
-    reviewRating: [
-        {
-            type: Schema.Types.ObjectId,
-            ref: 'reviewRating'
-        }
-    ]
-});
-
-
-const reviewRatingSchema = new Schema({
-    rating: Number,
-    review: {
-        type: Schema.Types.ObjectId,
-        ref: 'Review'
-    },
-    author: {
-        type: Schema.Types.ObjectId,
-        ref: "User"
-    }
-});
-
-
-
-
-module.exports = mongoose.model("reviewRating", reviewRatingSchema);
->>>>>>> 374e98b39449131a4f083e50c2e4870357400a25
 module.exports = mongoose.model("Review", reviewSchema);
--- routes/reviews.js
+++ routes/reviews.js
@@ -10,15 +10,8 @@ const { Review, reviewRating } = require('../models/review');
 router.post('/', isLoggedIn, reviewValidation, catchAsync(review.postReview))
 
 
-<<<<<<< HEAD
 router
   .delete("/:reviewId", isLoggedIn, isReviewAuthor, catchAsync(review.delete))
   .post("/:reviewId",isLoggedIn, catchAsync(review.postRating));
-=======
-
-router.route('/:reviewId')
-    .delete(isLoggedIn, isReviewAuthor, catchAsync(review.delete))
-    .post((review.reviewRating))
->>>>>>> 374e98b39449131a4f083e50c2e4870357400a25
 
 module.exports = router;
